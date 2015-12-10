39afae5f642360c878dd54feba3b9911be71923a
foxygun <foxygun@bk.ru>
2015-12-10 15:36:04 +0600

    2nd - initial

---
+++ src/com/epam/jamp/model/Person.java
@@ -0,0 +1,32 @@
+package com.epam.jamp.model;
+
+public class Person {
+
+    private String firstName;
+    private String secondName;
+    private long age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+}
