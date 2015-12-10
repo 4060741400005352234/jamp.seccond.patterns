ef5aee64151a935897c0ec54ca7ef69cf371af15
foxygun <foxygun@bk.ru>
2015-12-10 18:34:06 +0600

    2nd - first step (model, interfaces)

---
+++ src/com/epam/jamp/main/Main.java
@@ -0,0 +1,13 @@
+package com.epam.jamp.main;
+
+public class Main {
+
+//    try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("myfile.txt", true)))) {
+//        out.println("the text");
+//        //more code
+//        out.println("more text");
+//        //more code
+//    }catch (IOException e) {
+//        //exception handling left as an exercise for the reader
+//    }
+}
